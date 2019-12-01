407d3d6b3ecf62222faa3666129e040695415f14
arno <arnode125@gmail.com>
2019-12-02 00:32:24 +0800

    practice001

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,9 @@
 <?php
+session_start();
 $num1=10;
+$num3=8E-5;
+$name="Arno De";
+$_SESSION["name"] = $name;
 
 function myTest()
 {
@@ -17,5 +21,10 @@ echo "2 num2: $num2".PHP_EOL.PHP_EOL;
 echo <<<EOF
 this is $num1
 test
-EOF.PHP_EOL
+EOF.PHP_EOL;
+
+var_dump($num1);
+var_dump($num3);
+var_dump($name);
+
 ?>
---
+++ parameters.php
@@ -0,0 +1,32 @@
+<?php
+// session_start();
+include "./helloworld.php";
+
+if(42 == "42") {
+    echo "1 判断成功 42.".PHP_EOL;
+}else {
+    echo "1 判断失败 42.".PHP_EOL;
+}
+if(42 === "42") {
+    echo "2 判断成功 42.".PHP_EOL;
+}else {
+    echo "2 判断失败 42.".PHP_EOL;
+}
+
+// 定义常量，引用常量前面不能加$
+define("GREETING", "Hello, Arno De."); // 区分常量的大小写
+echo GREETING.PHP_EOL;
+// echo greeting.PHP_EOL; // 会报错
+define("GREETING2", "Hello, Arno De!", true); // 不区分常量的大小写
+echo GREETING2.PHP_EOL;
+echo greeting2.PHP_EOL;
+
+var_dump($name);
+echo strlen($name) . PHP_EOL;
+
+// 运算符
+echo 10 + 10 . PHP_EOL;
+echo "10dsf" + "10sss" . PHP_EOL;
+echo 10 . 10 . PHP_EOL;
+
+?>
