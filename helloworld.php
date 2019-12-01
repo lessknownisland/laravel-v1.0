c89653133a9be780df7ab2f6dbb4fc53328b222a
arno <arnode125@gmail.com>
2019-12-01 23:10:25 +0800

    111

--- helloworld.php
+++ helloworld.php
@@ -1,7 +1,21 @@
 <?php
-echo "Hello World!";
+$num1=10;
 
+function myTest()
+{
+    global $num1;
+    $num2=20;
+    echo "1 num1: $num1".PHP_EOL;
+    echo "1 num2: $num2".PHP_EOL;
+}
 
+myTest();
 
+echo "2 num1: $num1".PHP_EOL;
+echo "2 num2: $num2".PHP_EOL.PHP_EOL;
 
+echo <<<EOF
+this is $num1
+test
+EOF.PHP_EOL
 ?>
